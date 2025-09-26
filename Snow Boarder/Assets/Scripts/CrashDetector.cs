c35fa2fa06bf940c5dd51e46ef912b8b9fa692ca
Ege Biyikli <ege.biyikli1907@gmail.com>
2025-09-26 22:37:31 +0300

    Snow Boarder final project

---
+++ Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 0.5f;
+    [SerializeField] ParticleSystem crushEffect;
+    [SerializeField] AudioClip crashSFX;
+
+    bool hasCrashed = false;
+    void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Ground" && !hasCrashed) {
+            hasCrashed = true;
+            FindObjectOfType<PlayerController>().DisableControls();
+            crushEffect.Play();
+            GetComponent<AudioSource>().PlayOneShot(crashSFX);
+            Invoke("ReloadScene", loadDelay);
+        }
+    }
+    void ReloadScene() {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Snow Boarder/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 1f;
+    [SerializeField] float boostSpeed = 30f;
+    [SerializeField] float baseSpeed = 20f;
+
+    Rigidbody2D rb;
+    SurfaceEffector2D surfaceEffector2D;
+
+    bool canmove = true;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindObjectOfType<SurfaceEffector2D>();
+    }
+
+    void Update()
+    {
+        if (canmove) {
+            RotatePlayer();
+            RespondToBoost();
+        }
+    }
+
+    public void DisableControls() {
+        canmove = false;
+    }
+    void RespondToBoost() {
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+    void RotatePlayer() {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            rb.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) {
+            rb.AddTorque(-torqueAmount);
+        }
+    }
+}
